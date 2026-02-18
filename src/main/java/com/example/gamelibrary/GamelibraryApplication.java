73ec0d372931e027661c848f44afce715608b2f6
rottesy <egorkozel777@gmail.com>
2026-02-18 23:09:52 +0300

    add project structure

---
+++ src/main/java/com/example/gamelibrary/GamelibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamelibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamelibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamelibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamelibrary/GamelibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamelibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamelibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
